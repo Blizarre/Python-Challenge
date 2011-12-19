ef401684a01bb49785939c7acc217ea50d6f459e
Simon M <simon.marache@gmail.com>
2011-12-19 01:02:44 +0100

    Amélioration de la lisibilité

--- 15/challenge15.py
+++ 15/challenge15.py
@@ -1,4 +1,4 @@
-# http://www.pythonchallenge.com/pc/return/cat.html
+# http://www.pythonchallenge.com/pc/return/uzi.html
 
 import datetime as d
 
@@ -6,15 +6,15 @@ listeAnnees = []
 
 for diz in range(100):
     annee = 1006 + diz*10
-    if(d.date(annee,1,26).weekday()==0):    # does the 29 of january exist exist for this year ???
-                                            # Quick, Dirty and error-proof
+    if(d.date(annee,1,26).weekday()==0):    
         exist = True
         try:
-            d.date(annee,2,29).weekday()
+            d.date(annee,2,29).weekday() # does the 29 of january exist exist for this year ???
+                                         # Quick, Dirty and error-proof
         except Exception, e:
             exist = False
         if exist:
             listeAnnees.append(annee)
 
-print "Google me : 27 of january",  listeAnnees[-2]            
-                
+print "Google me : 27 of january",  listeAnnees[-2]  
+print "mozart !!"

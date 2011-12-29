1be31e0592a56188c674240e79f8a95a30c3a640
Simon M. <simon.marache@gmail.com>
2011-12-30 00:37:52 +0100

    Avancement challenge 17

--- 17/challenge17.py
+++ 17/challenge17.py
@@ -1,8 +1,8 @@
 # http://www.pythonchallenge.com/pc/return/romance.html
 
-import urllib2
+import urllib2, urllib
 
-urlCoo = "http://www.pythonchallenge.com/pc/return/cookies.html"
+urlCoo = "http://www.pythonchallenge.com/pc/def/linkedlist.php"
 #print urlCoo.info()
 
 aut=urllib2.HTTPBasicAuthHandler() 
@@ -13,4 +13,22 @@ uri="http://www.pythonchallenge.com",
 user='huge', 
 passwd='file') 
 opener=urllib2.build_opener(aut, urllib2.HTTPHandler(debuglevel=1), cook) 
-print opener.open(urlCoo)#.read() 
+print opener.open(urlCoo)
+
+# Cookie : info=you+should+have+followed+busynothing...;
+
+def getNext(number=""):
+	data_GET = urllib.urlencode({"busynothing":number})
+	resultat = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?" + data_GET)
+	print resultat.headers["Set-Cookie"]
+	resultat = resultat.read()
+	print resultat
+	return float(resultat.split()[-1])
+
+
+currentNum = getNext()
+
+while(currentNum):
+	currentNum = getNext(currentNum)
+	
+	
